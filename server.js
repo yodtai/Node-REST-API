2fe21f7036f8e7ece9b32de1c358b8fdd9a6219f
Anirach Mingkhwan <Anirach.m@fitm.kmutnb.ac.th>
2021-03-14 21:48:22 +0700

    code done-i

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://admin:1234@cluster0.co4mq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true })
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
