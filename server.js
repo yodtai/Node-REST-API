4f09c4dffdcb37bb77f6d44b1c4fa81892d49e72
Your Name <you@example.com>
2021-03-17 18:40:30 +0700

    test

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://admin:1234@cluster0.co4mq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const DATABASE_URL = process.env.DATABASE_URL || "mongodb+srv://admin:12345@cluster0.2rf7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
